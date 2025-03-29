7dcfad23b878be7db60b56d2351f259a77084e14
Aishwarya <toaishupatil@gmail.com>
2025-03-30 04:51:02 +0530

    Add krishimitra/lib/core/providers/locale_provider.dart 1743290461545

---
+++ krishimitra/lib/core/providers/locale_provider.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class LocaleProvider extends ChangeNotifier {
+  Locale _locale = const Locale('en');
+
+  Locale get locale => _locale;
+
+  void setLocale(Locale newLocale) {
+    if (_locale != newLocale) {
+      _locale = newLocale;
+      notifyListeners();
+    }
+  }
+}
